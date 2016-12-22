fab7bbcc13a2b82553a064470b1b023997b5125b
firmanheriansyah <heriansyah.firman@gmail.com>
2016-12-22 22:13:59 +0700

    Initial commit

---
+++ app/src/main/java/com/video/herians/video/MainActivity2.java
@@ -0,0 +1,36 @@
+package com.video.herians.video;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity2 extends Activity {
+    VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Project Video
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        videoView = (VideoView) findViewById(R.id.videoView);
+        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.teaser));
+        videoView.setMediaController(new MediaController(this));
+        videoView.requestFocus();
+        //agar video start otomatis
+
+        videoView.start();
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+}
